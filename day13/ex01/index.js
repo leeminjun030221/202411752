08ca11c8c7374ca84d4e4637b3329ff5dae5b94c
leeminjun030221 <minjun000221@gmail.com>
2025-11-26 10:48:08 +0900

    13-2

--- day13/ex01/index.js
+++ day13/ex01/index.js
@@ -3,6 +3,7 @@ export default async function main() {
 
     const mainMenu = document.querySelector('#main-menu')
     const creditScreen = document.querySelector('#credit-screen')
+    const optionScreen = document.querySelector('#option-screen')
     let currentScreen = "mainMenu"
     
     let currentIndex = 0;
@@ -38,6 +39,11 @@ export default async function main() {
                     creditScreen.classList.remove('hide')
                     currentScreen = "creditScreen"
                 }
+                 else if(select_action == 'option') {        // ⬅ 옵션 화면 열기 추가
+                    mainMenu.classList.add('hide')
+                    optionScreen.classList.remove('hide')
+                    currentScreen = "optionScreen"
+                }
             }
 
             console.log(currentIndex)
@@ -50,6 +56,13 @@ export default async function main() {
 
             if(e.key == "Enter") {
                 creditScreen.classList.add('hide')
+                mainMenu.classList.remove('hide')
+                currentScreen = "mainMenu"
+            }
+        }
+        else if(currentScreen == "optionScreen") {
+            if(e.key == "Enter") {
+                optionScreen.classList.add('hide')
                 mainMenu.classList.remove('hide')
                 currentScreen = "mainMenu"
             }
